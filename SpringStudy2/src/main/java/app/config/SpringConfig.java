87108becbd7dd077b6ca6bfb2bdda375e0897d1e
JJAJJAKIM <csm9121@gmail.com>
2024-06-17 10:19:08 +0900

    Spring Framework로 web.xml 설정 처리

---
+++ SpringStudy2/src/main/java/app/config/SpringConfig.java
@@ -0,0 +1,32 @@
+package app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc // web.xml에서 어노테이션 사용관련 설정과 동일하다.
+@ComponentScan(basePackages = {"app"})  //web.xml의 
+public class SpringConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		
+		InternalResourceViewResolver ivs = new InternalResourceViewResolver();
+		ivs.setPrefix("/WEB-INF/views/");
+		ivs.setSuffix(".jsp");
+		registry.viewResolver(ivs);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resource/**").addResourceLocations("/resource/"); //web.xml의 화면단 파일과 URL매핑
+	}
+
+	
+	
+}
